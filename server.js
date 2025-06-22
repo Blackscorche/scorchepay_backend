97b8832362c9ff2ef927923aba886b2a6aa10467
Blackscorche <168141352+Blackscorche@users.noreply.github.com>
2025-06-22 07:05:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import walletRoutes from "./routes/WalletRoutes.js";
+import walletRoutes from "./routes/walletRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 
